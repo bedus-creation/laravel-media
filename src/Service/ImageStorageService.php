c0c5bc307b2f7dbd7f2f545d90f66154cfd48a28
Ved <tmgbedu@gmail.com>
2021-04-25 01:11:18 +0545

    Fix blurred image issue #13

--- src/Service/ImageStorageService.php
+++ src/Service/ImageStorageService.php
@@ -8,6 +8,11 @@ use Intervention\Image\ImageManagerStatic as Image;
 use Illuminate\Http\File;
 use Illuminate\Http\UploadedFile;
 
+/**
+ * Class ImageStorageService
+ *
+ * @package Aammui\LaravelMedia\Service
+ */
 class ImageStorageService extends BaseStorageService implements FileStorageInterface
 {
     /**
@@ -25,16 +30,17 @@ class ImageStorageService extends BaseStorageService implements FileStorageInter
     }
 
     /**
-     * @param $image
-     * @param $key
-     * @param $path
-     * @param $disk
+     * @param File|UploadedFile $file
+     * @param                   $key
+     * @param                   $path
+     * @param                   $disk
      */
-    private function resizeImage($image, $key, $path, $disk)
+    private function resizeImage($file, $key, $path, $disk)
     {
+        $image = Image::make($file);
+
         $img = $image->resize(
-            Responsive::getWidthFromConfig($key),
-            Responsive::getHeightFromConfig($key),
+            Responsive::getWidthFromConfig($key), Responsive::getHeightFromConfig($key),
             function ($constraint) {
                 $constraint->aspectRatio();
             }
@@ -51,17 +57,14 @@ class ImageStorageService extends BaseStorageService implements FileStorageInter
      */
     private function prepareURL($file, string $disk): array
     {
-        $fileHash = $file->hashName();
-
-        $image = Image::make($file);
-
         $sizes = Responsive::getConfig();
 
         return collect($sizes)->map(
-            function ($value, $key) use ($image, $fileHash, $disk) {
-                $path = $key."/".$fileHash;
+            function ($value, $key) use ($file, $disk) {
 
-                $this->resizeImage($image, $key, $path, $disk);
+                $path = $key."/".$file->hashName();;
+
+                $this->resizeImage($file, $key, $path, $disk);
 
                 return "/".$path;
             }
